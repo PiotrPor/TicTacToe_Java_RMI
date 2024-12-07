e3fc49cd2dac6c120e8264868208dcfbd0de50f0
PiotrPor <piotr.porembinski@outlook.com>
2024-12-07 16:21:04 +0100

    classes for Server and Client

---
+++ src/TicTacToeClient.java
@@ -0,0 +1,156 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class TicTacToeClient extends JFrame {
+    private int ID; //TODO: can be private as well?
+    private TicTacToeInterfServer server;
+    private JButton[][] buttons;
+    private JLabel labelForPlayer;
+    private char playerSign;
+    private char[][] board;
+    private boolean gameStarted;
+    private boolean isMyTurn;
+
+    public TicTacToeClient() {
+        int buttonSize = 80;
+        int marginSize = 20;
+        int heightOfLabel = 30;
+        int totalWindowWidth = 3*buttonSize+2*marginSize;
+        int totalWindowHeight = 4*buttonSize+4*marginSize+heightOfLabel;
+        setTitle("Tic-Tac-Toe Game");
+        setSize(totalWindowWidth, totalWindowHeight);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(null);
+        buttons = new JButton[3][3];
+        playerSign = '-';
+        gameStarted = false;
+        isMyTurn = false;
+
+        // add the JLabel component
+        labelForPlayer = new JLabel("Creating...");
+        labelForPlayer.setEnabled(true);
+        labelForPlayer.setBounds(0,marginSize,totalWindowWidth,heightOfLabel);
+        labelForPlayer.setVisible(true);
+        add(labelForPlayer);
+        // Initialize buttons for the Tic-Tac-Toe grid
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j] = new JButton("-");
+                buttons[i][j].setFont(new Font("Arial", Font.PLAIN, 40));
+                buttons[i][j].setFocusPainted(false);
+                buttons[i][j].setEnabled(true); //TODO: maybe wait until there are 2 players?
+                buttons[i][j].setBounds(
+                        j*buttonSize+marginSize,
+                        i*buttonSize+2*marginSize+heightOfLabel,
+                        buttonSize,
+                        buttonSize
+                        ); //x y width height
+                buttons[i][j].addActionListener(new ButtonClickListener(i, j));
+                add(buttons[i][j]);
+            }
+        }
+        // Register the client on the server
+        labelForPlayer.setText("Created");
+        System.out.println("Client's constructor finished.");
+    }
+
+    public void registerYourself() {
+        System.out.println("Client will try to register on the server.");
+        try {
+            // Look up the server using RMI
+            server = (TicTacToeInterfServer) Naming.lookup("//localhost/TicTacToe");
+            playerSign = server.registerPlayer(new ClientCallback());
+            System.out.println("Client registered with sign \""+playerSign+"\" and ID="+ID);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class ButtonClickListener implements ActionListener {
+        private int row, col;
+
+        public ButtonClickListener(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("Pressed button "+row+";"+col); //debug!!
+            System.out.println("  gameStarted: "+gameStarted+" , isMyTurn: "+isMyTurn);
+            if (gameStarted && isMyTurn) {
+                try {
+                    System.out.println("  Will invoke makeMove() method on the server");
+                    if (server.makeMove(row, col, ID)) {
+                        System.out.println("  Server added the new sign onto the board");
+                        buttons[row][col].setEnabled(false);
+                        isMyTurn = false;
+                    }
+                } catch (RemoteException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private class ClientCallback extends UnicastRemoteObject implements TicTacToeInterfClient {
+        public ClientCallback() throws RemoteException {
+            super();
+        }
+
+        @Override
+        public void updateStatus(String message) throws RemoteException {
+            System.out.println("Client updateStatus() \""+message+"\". isMyTurn: "+isMyTurn);
+            SwingUtilities.invokeLater(() -> {
+                //TODO: later use "JOptionPane.showMessageDialog(this, message)" ??
+                TicTacToeClient.this.labelForPlayer.setText(message);
+            });
+        }
+
+        @Override
+        public void updateBoard(char[][] updatedBoard) throws RemoteException {
+            SwingUtilities.invokeLater(() -> {
+                // Update the client's board with the new state from the server
+                TicTacToeClient.this.board = updatedBoard;
+                // Update the visual buttons to reflect the new board state
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        TicTacToeClient.this.buttons[i][j].setText(String.valueOf(updatedBoard[i][j]));
+                    }
+                }
+            });
+        }
+
+        @Override
+        public void makeThemStartPlaying(Boolean doesHeBegin) throws RemoteException {
+            gameStarted = true;
+            isMyTurn = doesHeBegin;
+            System.out.println("Server makes you start playing. isMyTurn: "+isMyTurn);
+        }
+
+        @Override
+        public void tellItsTheirTurn(Boolean isItTheirTurn) throws RemoteException {
+            isMyTurn = isItTheirTurn;
+            System.out.println("Server tells you about your turn. isMyTurn: "+isMyTurn);
+        }
+
+        @Override
+        public void setID(int newValue) throws RemoteException {
+            ID = newValue;
+        }
+
+        @Override
+        public int getID() throws RemoteException {
+            return ID;
+        }
+
+        @Override
+        public void proveRemoteInvocation() throws RemoteException {
+            System.out.println("Method in ClientCallback has been remotely invoked by RMI.");
+        }
+    }
+}
---
+++ src/TicTacToeServer.java
@@ -0,0 +1,83 @@
+import java.rmi.server.UnicastRemoteObject;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TicTacToeServer extends UnicastRemoteObject implements TicTacToeInterfServer {
+    private int howManyPlayersJoined;
+    private List<TicTacToeInterfClient> players;
+    private List<GameSession> gameSessions;
+
+    // Constructor
+    public TicTacToeServer() throws RemoteException {
+        super();
+        howManyPlayersJoined = 0;
+        players = new ArrayList<>();
+        gameSessions = new ArrayList<>();
+    }
+
+    @Override
+    public boolean makeMove(int row, int col, int moverID) throws RemoteException {
+        System.out.println("Player "+moverID+" wants to put a sign on "+row+" ; "+col);
+        boolean successful = false;
+        for(GameSession gra : gameSessions) {
+            if(gra.isThisPlayerHere(moverID) > 0) {
+                System.out.println("  Found the game where he is");
+                successful = gra.performMove(row, col, moverID);
+                break;
+            }
+        }
+        return successful;
+    }
+
+    @Override
+    public char registerPlayer(TicTacToeInterfClient player) throws RemoteException {
+        System.out.println("Server tries to register a new player");
+        char assignedSign = '-';
+        howManyPlayersJoined += 1;
+        player.setID(howManyPlayersJoined);
+        players.add(player);
+        if (gameSessions.isEmpty() || areAllSessionsFull()) {
+            System.out.println("  Creating a new game session");
+            GameSession newGame = new GameSession();
+            newGame.addPlayer(player);
+            gameSessions.add(newGame);
+            assignedSign = 'X';
+            player.updateStatus("Waiting for the second player to join");
+        } else {
+            System.out.println("  Adding player to the existing game session");
+            for(GameSession gra : gameSessions) {
+                if (gra.tellHowManyPlayers() < 2) {
+                    assignedSign = gra.addPlayer(player);
+
+                    if(assignedSign == 'X') {
+                        System.out.println("  registered as 1st player");
+                    } else if(assignedSign == 'O') {
+                        System.out.println("  registered as 2nd player");
+                    }
+
+                    if(gra.tellHowManyPlayers() == 2) {
+                        System.out.println("  Because there are 2 players registered it's time to start the game");
+                        gra.startGame();
+                    }
+                    break;
+                }
+            }
+        }
+        return assignedSign;
+    }
+
+    private boolean areAllSessionsFull() {
+        for(GameSession gra : gameSessions) {
+            if (gra.tellHowManyPlayers() < 2) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void proveRemoteInvocation() throws RemoteException {
+        System.out.println("Method on the server has been remotely invoked by RMI.");
+    }
+}
