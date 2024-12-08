e34ad5f453f5a2120758f640c4c1f7391cc5c274
PiotrPor <piotr.porembinski@outlook.com>
2024-12-08 11:00:55 +0100

    Resized components of Client window

--- src/ClientClass.java
+++ src/ClientClass.java
@@ -13,18 +13,18 @@ public class ClientClass extends JFrame {
     private JLabel labelAboutOpponent;
     private JButton buttonToExit;
     private char playerSign;
-    private char[][] board;
     private boolean gameStarted;
     private boolean isMyTurn;
 
     public ClientClass() {
         int gameButtonSize = 80;
-        int marginSize = 20;
+        int horizontalMargin = 40;
+        int verticalMargin = 20;
         int heightOfLabel = 30;
         int exitButtonWidth = (int)(2*gameButtonSize); //120? 160?
         int exitButtonHeight = 50;
-        int totalWindowWidth = 3*gameButtonSize+3*marginSize;
-        int totalWindowHeight = 4*gameButtonSize+4*marginSize+2*heightOfLabel;
+        int totalWindowWidth = 3*gameButtonSize+(2+1)*horizontalMargin;
+        int totalWindowHeight = 4*gameButtonSize+4*verticalMargin+2*heightOfLabel;
         setTitle("Tic-Tac-Toe Java RMI Client");
         setSize(totalWindowWidth, totalWindowHeight);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
@@ -37,14 +37,30 @@ public class ClientClass extends JFrame {
         // add the JLabel component
         labelForPlayer = new JLabel("Creating...");
         labelForPlayer.setEnabled(true);
-        labelForPlayer.setBounds(0,marginSize,totalWindowWidth,heightOfLabel);
+        labelForPlayer.setFont(new Font(
+                "Arial",
+                Font.PLAIN,
+                (int)(0.7*heightOfLabel) ));
+        labelForPlayer.setBounds(
+                0,
+                verticalMargin,
+                totalWindowWidth,
+                heightOfLabel);
         labelForPlayer.setHorizontalAlignment(SwingConstants.CENTER);
         labelForPlayer.setVisible(true);
         add(labelForPlayer);
         //second JLabel
         labelAboutOpponent = new JLabel(" ");
         labelAboutOpponent.setEnabled(true);
-        labelAboutOpponent.setBounds(0,2*marginSize,totalWindowWidth,heightOfLabel);
+        labelAboutOpponent.setFont(new Font(
+                "Arial",
+                Font.PLAIN,
+                (int)(0.7*heightOfLabel) ));
+        labelAboutOpponent.setBounds(
+                0,
+                verticalMargin+heightOfLabel,
+                totalWindowWidth,
+                heightOfLabel);
         labelAboutOpponent.setHorizontalAlignment(SwingConstants.CENTER);
         labelAboutOpponent.setVisible(true);
         add(labelAboutOpponent);
@@ -53,12 +69,15 @@ public class ClientClass extends JFrame {
         for (a = 0; a < 3; a++) {
             for (b = 0; b < 3; b++) {
                 buttons[a][b] = new JButton("-");
-                buttons[a][b].setFont(new Font("Arial", Font.PLAIN, 40));
+                buttons[a][b].setFont(new Font(
+                        "Arial",
+                        Font.PLAIN,
+                        (int)(0.7*gameButtonSize) ));
                 buttons[a][b].setFocusPainted(false);
                 buttons[a][b].setEnabled(false); //TODO: maybe wait until there are 2 players?
                 buttons[a][b].setBounds(
-                        b*gameButtonSize+marginSize,
-                        a*gameButtonSize+2*marginSize+2*heightOfLabel,
+                        b*gameButtonSize+horizontalMargin,
+                        a*gameButtonSize+2*verticalMargin+2*heightOfLabel,
                         gameButtonSize,
                         gameButtonSize
                         ); //x y width height
@@ -68,12 +87,15 @@ public class ClientClass extends JFrame {
         }
         // add JButton for leavin the game
         buttonToExit = new JButton("Exit");
-        buttonToExit.setFont(new Font("Arial", Font.PLAIN, 30));
+        buttonToExit.setFont(new Font(
+                "Arial",
+                Font.PLAIN,
+                (int)(0.7*exitButtonHeight) ));
         buttonToExit.setVisible(false); //for now
         buttonToExit.setEnabled(false); //for now
         buttonToExit.setBounds(
-                marginSize+gameButtonSize/2,
-                3*gameButtonSize+3*marginSize+2*heightOfLabel,
+                horizontalMargin+gameButtonSize/2,
+                3*gameButtonSize+3*verticalMargin+2*heightOfLabel,
                 exitButtonWidth,
                 exitButtonHeight
                 );
@@ -154,9 +176,6 @@ public class ClientClass extends JFrame {
 
         @Override
         public void updateBoard(char[][] updatedBoard) throws RemoteException {
-            // Update the client's board with the new state from the server
-            ClientClass.this.board = updatedBoard;
-            // Update the visual buttons to reflect the new board state
             int a,b;
             for (a = 0; a < 3; a++) {
                 for (b = 0; b < 3; b++) {
